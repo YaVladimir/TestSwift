777fb8186d10b7b86bb51c561766cf1900c136c9
YaVladimir <Elix270295>
2018-06-21 20:58:34 +0300

    Initial Commit

---
+++ test swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  test swift
+//
+//  Created by Vladimir Yakovenko on 21.06.2018.
+//  Copyright © 2018 Vladimir Yakovenko. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
